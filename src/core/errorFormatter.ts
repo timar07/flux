75af2ef37f87adab349b753b1ddf88d8c15a265d
timofey_arkhipov <timar@hotmail.com>
2023-07-29 22:15:02 +0300

    add `ErrorFormatter` for formatting error messages

---
+++ src/core/errorFormatter.ts
@@ -0,0 +1,12 @@
+import DescribableError from "./describableError";
+
+export default class ErrorFormatter {
+    constructor(
+        private error: DescribableError
+    ) {}
+
+    public toString() {
+        const pos = this.error.getDebugInfo().getPosition();
+        return `${this.error.getType()}: ${this.error.getMessage()} [${pos.line}:${pos.col}]`;
+    }
+}
