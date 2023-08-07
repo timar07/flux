fa2340869d1c5003d3c79cc6cdd4549867104d61
timofey_arkhipov <timar@hotmail.com>
2023-08-07 21:00:54 +0300

    wire up `SourceReader` and `CLIArgsParser`

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
+import { CLIArgsParser } from './core/cliArgsParser';
+import SourceReader from './core/sourceReader';
 import DescribableError from './core/describableError';
 import ErrorFormatter from './core/errorFormatter';
 import { Lexer } from './lexer/lexer';
 import { TokenTag } from './lexer/token';
 
 try {
-    new Lexer('(){}.,;^=><+-*/!').getTokens().forEach(token => {
+    const cliArgs = new CLIArgsParser().parse();
+    const reader = new SourceReader();
+
+    console.log(cliArgs);
+
+    new Lexer(reader.readFile(cliArgs.fileName)).getTokens().forEach(token => {
         console.log(TokenTag[token.getTag()] + ' ' + token.getPosition());
     });
 } catch (e) {
