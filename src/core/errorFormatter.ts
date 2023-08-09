48a3be5e4c7069383875cc94fc14c1cc55a63fc3
timofey_arkhipov <timar@hotmail.com>
2023-08-09 21:32:40 +0300

    colorize error type

--- src/core/errorFormatter.ts
+++ src/core/errorFormatter.ts
@@ -7,6 +7,10 @@ export default class ErrorFormatter {
 
     public toString() {
         const pos = this.error.getDebugInfo().getPosition();
-        return `${this.error.getType()}: ${this.error.getMessage()} [${pos.line}:${pos.col}]`;
+        return `${this.getErrorTypeString()}: ${this.error.getMessage()} [${pos.line}:${pos.col}]`;
+    }
+
+    private getErrorTypeString() {
+        return `\x1b[31m${this.error.getType()}\x1b[0m`;
     }
 }
